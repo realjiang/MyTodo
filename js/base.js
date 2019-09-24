c0abf3ffc4ae6604e85bd9c6a41a60f335cddd4c
superjishere <superjishere@163.com>
2019-09-24 20:31:30 +0800

    详情页面的显示和隐藏

--- js/base.js
+++ js/base.js
@@ -5,12 +5,16 @@
   'use strict';
 
   var $form_add_task = $('.add-task'),
-    $delete_task,
+    $task_delete_trigger,
+    $task_detail_trigger,
+    $task_detail = $('.task-detail'),
+    $task_detail_mask = $('.task-detail-mask'),
     task_list = [];
 
   init();
 
   $form_add_task.on('submit',on_add_task_form_submit);
+  $task_detail_mask.on('click',hide_task_detail);
   function on_add_task_form_submit(e){
     var   new_task = {};
     //禁用默认行为
@@ -25,18 +29,61 @@
       $input.val(null);
     }
   }
+  function listen_task_detail(){
+    $task_detail_trigger.on('click',function () {
+      var $this = $(this);
+      var $item = $this.parent().parent();
+      var index = $item.data('index');
+      show_task_detail(index);
+    })
+  }
+  //点击详情，弹出详情页
+  function show_task_detail(index) {
+    render_task_detail(index);
+    $task_detail.show();
+    $task_detail_mask.show();
+  }
+  //点击mask部分，隐藏详情页
+  function hide_task_detail() {
+    $task_detail.hide();
+    $task_detail_mask.hide();
+  }
+  //渲染指定任务列表的详细信息
+  function render_task_detail(index) {
+    if(index === undefined || !task_list[index]) return;
+    var item = task_list[index];
+    // console.log('item',item);
+    var tpl =
+        '<div>'+
+          '<div class="content">'+
+          item.content +
+          '</div>'+
+          '<div>'+
+          '<div class="desc">'+
+          '<textarea value ="' + item.desc + '"></textarea>'+
+          '</div>'+
+          '</div>'+
+          '<div class="remind"> '+
+          '<input type="date">'+
+          '<button type="submit">submit</button>'+
+          '</div>'+
+        '</div>';
+
+    $task_detail.html(null);
+    $task_detail.html(tpl);
+  }
   //单独封装到一个函数中，init阶段调用，解决不能持续监听（只能删除第一次点击的）的问题
   //因为jQuery不会自动绑定数据和view的变化，需要我们手动将变化的内容添加到监控源
   //查找并监听所有删除按钮的点击事件
   function listen_task_delete(){
-    $delete_task.on('click',function () {
+    $task_delete_trigger.on('click',function () {
       var $this = $(this);
       //找到删除按钮所在的task元素
       var $item = $this.parent().parent();
       //确认删除
       var tmp = confirm('确定删除？');
       var index = $item.data('index');
-      tmp ? delete_task(index) : null;
+      tmp ? task_delete(index) : null;
     })
   }
 
@@ -55,7 +102,7 @@
     render_task_list();
   }
   //删除一条task
-  function delete_task(index){
+  function task_delete(index){
     //如果没有index或者index不存在则直接返回
     if(index === undefined || !task_list[index]) return;
     delete task_list[index];
@@ -77,8 +124,10 @@
        var $task = render_task_item(task_list[i],i);
        $task_list.append($task);
     }
-    $delete_task = $('.action.delete');
+    $task_delete_trigger = $('.action.delete');
+    $task_detail_trigger = $('.action.detail');
     listen_task_delete();
+    listen_task_detail();
   }
   //渲染单条task模板
   function render_task_item(data,index){
