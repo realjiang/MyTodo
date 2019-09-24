51a27ab4e6264449490b1aa39465e98b7a64ca34
superjishere <superjishere@163.com>
2019-09-24 22:56:45 +0800

    详情页面的更新

--- js/base.js
+++ js/base.js
@@ -9,7 +9,12 @@
     $task_detail_trigger,
     $task_detail = $('.task-detail'),
     $task_detail_mask = $('.task-detail-mask'),
-    task_list = [];
+    $task_detail_content,
+    $task_detail_content_input,
+    task_list = [],
+    current_index,
+    $update_form
+    ;
 
   init();
 
@@ -29,7 +34,13 @@
       $input.val(null);
     }
   }
+  //监听打开task详情的事件
   function listen_task_detail(){
+    var index;
+    $('.task-item').on('dblclick',function () {
+      index = $(this).data('index');
+      show_task_detail(index);
+    });
     $task_detail_trigger.on('click',function () {
       var $this = $(this);
       var $item = $this.parent().parent();
@@ -40,8 +51,17 @@
   //点击详情，弹出详情页
   function show_task_detail(index) {
     render_task_detail(index);
+    current_index = index;
+    //显示详情模板，默认隐藏
     $task_detail.show();
+    //显示详情mask模板，默认隐藏
     $task_detail_mask.show();
+  }
+  //更新task
+  function update_task(index,data){
+    if(!index || !task_list[index]) return;
+    task_list[index] = data;/*$.merge({},task_list[index],data);*/
+    refresh_task_list();
   }
   //点击mask部分，隐藏详情页
   function hide_task_detail() {
@@ -54,23 +74,46 @@
     var item = task_list[index];
     // console.log('item',item);
     var tpl =
-        '<div>'+
+        '<form>'+
           '<div class="content">'+
-          item.content +
+        (item.content || '')+
           '</div>'+
+          '<div class="input-item"><input style="display: none" type="text" name="content" value="' + item.desc + '"></div>'+
           '<div>'+
-          '<div class="desc">'+
-          '<textarea value ="' + item.desc + '"></textarea>'+
+          '<div class="desc input-item">'+
+          '<textarea name ="desc">' + (item.desc || '') + '</textarea>'+
           '</div>'+
           '</div>'+
-          '<div class="remind"> '+
-          '<input type="date">'+
-          '<button type="submit">submit</button>'+
+          '<div class="remind input-item"> '+
+          '<input class="datetime" name="remind_date" type="date" value="' + item.remind_date + '">'+
           '</div>'+
-        '</div>';
-
+          '<div class="input-item"><button type="submit">更新</button></div>'+
+        '</form>';
+    //清空task详情旧模板
     $task_detail.html(null);
+    //添加新模板，用新模板替换
     $task_detail.html(tpl);
+    /*选中其中的form元素, 因为之后会使用其监听submit事件*/
+    $update_form = $task_detail.find('form');
+    /*选中显示Task内容的元素*/
+    $task_detail_content = $update_form.find('.content');
+    /*选中Task input的元素*/
+    $task_detail_content_input =  $update_form.find('[name=content]');
+    /*双击内容元素显示input, 隐藏自己*/
+    $task_detail_content.on('dblclick',function () {
+      $task_detail_content_input.show();
+      $task_detail_content.hide();
+    })
+    $update_form.on('submit',function (e) {
+      e.preventDefault();
+      var data = {};
+      //获取表单中各个input的值
+      data.content = $(this).find('[name = content]').val();
+      data.desc = $(this).find('[name = desc]').val();
+      data.remind_date = $(this).find('[name = remind_date]').val();
+      update_task(index,data);
+      hide_task_detail();
+    })
   }
   //单独封装到一个函数中，init阶段调用，解决不能持续监听（只能删除第一次点击的）的问题
   //因为jQuery不会自动绑定数据和view的变化，需要我们手动将变化的内容添加到监控源
@@ -122,7 +165,7 @@
     $task_list.html('');
     for(var i = 0;i < task_list.length;i++){
        var $task = render_task_item(task_list[i],i);
-       $task_list.append($task);
+       $task_list.prepend($task);
     }
     $task_delete_trigger = $('.action.delete');
     $task_detail_trigger = $('.action.detail');
